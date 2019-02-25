37aeb936a21787be280eb4c120b01685703d019e
Puyodead1 <23562356riley@gmail.com>
2019-02-25 18:04:17 -0500

    Updates

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const Discord = require("discord.js"),
   mongoose = require("mongoose"),
   Enmap = require("enmap"),
   statuses = require("./statuses.json");
+const wait = require("util").promisify(setTimeout);
 
 client.commands = new Enmap();
 client.commandNames = [];
 client.admins = require("./admins.json");
-
+const invites = {};
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
@@ -50,6 +51,14 @@ db.once("open", async () => {
 });
 
 client.on("ready", () => {
+  wait(1000);
+  // Load all invites for all guilds and save them to the cache.
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+
   console.log(`Logged in as ${client.user.tag}!`);
 
   const status = statuses[Math.floor(Math.random() * statuses.length)];
@@ -61,13 +70,66 @@ client.on("ready", () => {
     }, 7200000);
   });
 });
-
+client.on("guildMemberAdd", member => {
+  // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const logChannel = member.guild.channels.find(
+      channel => channel.name === "join-logs"
+    );
+    if (
+      invite.uses >= 10 &&
+      !client.guilds
+        .get("473726968651710464")
+        .members.get(inviter.id)
+        .roles.has("542476601133105153")
+    ) {
+      client.guilds
+        .get("473726968651710464")
+        .channels.get("473726968651710466")
+        .send(
+          `Congrats ${inviter}, you have invited 10 or more people! You have been granted the Staff role!`
+        );
+    }
+    // A real basic message with the information we need.
+    logChannel.send(
+      `${member.user.tag} joined using invite code \`\`${
+        invite.code
+      }\`\` from ${inviter.tag}. Invite was used \`\`${invite.uses}\`\` times.`
+    );
+  });
+});
 client.on("message", async msg => {
-  if (msg.channel.type === "dm")
-    client
-      .fetchUser("213247101314924545")
-      .send(`From ${msg.author.username} \`\`\`${msg.content}\`\`\``);
-
+  if (msg.channel.type === "dm") {
+    if (msg.content.startsWith("set")) {
+      const args = msg.content.trim().split(/ +/g);
+      const command = args.shift().toLowerCase();
+      client.pmforwardChannel = args[0];
+      return msg.channel.send(
+        `Set the channel id to ${client.pmforwardChannel}`
+      );
+    }
+    if (msg.author.id === "213247101314924545") {
+      if (!client.pmforwardChannel) {
+        return client.guilds
+          .get("473726968651710464")
+          .channels.get("473726968651710466")
+          .send(msg.content);
+      }
+      return client.guilds
+        .get("473726968651710464")
+        .channels.get(client.pmforwardChannel)
+        .send(msg.content);
+    }
+  }
   if (msg.author.id === "541378318151188491") {
     console.log(`Recieved notification`);
     // Forward it
@@ -94,6 +156,9 @@ client.on("message", async msg => {
     return console.log("Done");
   }
   if (msg.author.bot) return;
+  if (msg.content.toLowerCase().includes("whore")) {
+    return msg.reply("no u bitch");
+  }
   if (msg.content.toLowerCase().indexOf("o!") !== 0) return;
   const args = msg.content
     .slice(2)
